595759a881594275ce38006e40fea8fc5684a2b1
Sergey <sergproblem@gmail.com>
2024-11-22 19:55:05 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+import tkinter as tk
+
+from pdf_search_app import PDFSearchApp
+
+# Ensure stdout supports UTF-8 encoding
+sys.stdout.reconfigure(encoding='utf-8')
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PDFSearchApp(root)
+    root.mainloop()
