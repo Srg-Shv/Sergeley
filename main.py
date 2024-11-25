d4c1887eb171b8f601a5df6069ed31b0eb1f536f
Sergey <sergproblem@gmail.com>
2024-11-25 20:21:25 +0400

    3.1

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import sys
+import logging
 import tkinter as tk
 
 from pdf_search_app import PDFSearchApp
+
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+# Main logger
+logger = logging.getLogger(__name__)
 
 # Ensure stdout supports UTF-8 encoding
 sys.stdout.reconfigure(encoding='utf-8')
