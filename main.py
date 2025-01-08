9707a798213cd67c0a5d30438ba8dfa7e56031e8
Sergey <sergproblem@gmail.com>
2025-01-08 13:07:17 +0400

    3.4

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+#Sergeley 3.4
 import sys
-import logging
 import tkinter as tk
 
 from pdf_search_app import PDFSearchApp
 
-# Ensure stdout supports UTF-8 encoding
 sys.stdout.reconfigure(encoding='utf-8')
 
 if __name__ == "__main__":
